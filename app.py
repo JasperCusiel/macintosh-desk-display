70855360a6a0d5bf4547f47300f7c21c67931354
Jasper Cusiel <jaspercusiel@icloud.com>
2023-06-19 19:43:12 +1200

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import eel
 import spotipy
 
@@ -7,6 +8,55 @@ except ImportError:
     print("Error: spotipy library not found")
 
 @eel.expose
+=======
+# import lyricsgenius as lg
+import spotipy
+import time
+import json
+import os
+from flask import Flask, jsonify, render_template
+
+SPOTIPY_CLIENT_ID = "73404788fd19469ba44c59f7130e5b28"
+SPOTIPY_CLIENT_SECRET = "0fdbfc8a347d4cc3be81f36c80fe31d2"
+SPOTIPY_REDIRECT_URI = "https://google.com"
+GENIUS_ACCESS_TOKEN = "Okbi1ioN5ofQ0P8B5XE2E9SUK7pU26cc8bvxsTIfRdlviikynk5elU_HSK4EDvCp"
+
+scope = "user-read-currently-playing user-read-playback-state"
+
+oauth_object = spotipy.SpotifyOAuth(
+    client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET,
+    redirect_uri=SPOTIPY_REDIRECT_URI,
+    scope=scope,
+)
+token = None  # Define a default value
+token_dict = oauth_object.get_access_token()
+if token_dict is not None:
+    token = token_dict["access_token"]
+    # Use the token to make API requests
+else:
+    # Handle the case where get_access_token() returned None
+    print("Error: get_access_token() returned None")
+
+# Spotify Object
+spotify_object = spotipy.Spotify(auth=token)
+
+# Genius Object
+# genius = lg.Genius(GENIUS_ACCESS_TOKEN)
+
+current = spotify_object.currently_playing()
+# print(json.dumps(playing, sort_keys=False, indent=4))
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/currently_playing_info")
+>>>>>>> 32224c1 (update)
 def get_image_url():
     current_track = spotify_object.current_playback()
     if current_track is not None and current_track["is_playing"]:
@@ -18,7 +68,10 @@ def get_image_url():
             length = current["item"]["duration_ms"]
             progress = current["progress_ms"]
             album_art_url = current["item"]["album"]["images"][0]["url"]
+<<<<<<< HEAD
             # print(album_art_url)
+=======
+>>>>>>> 32224c1 (update)
 
             # Create a dictionary with the desired values
             data = {
@@ -42,6 +95,7 @@ def get_image_url():
             "is_playing": False
         }
 
+<<<<<<< HEAD
     # Return the dictionary
     return data
 
@@ -69,3 +123,42 @@ if __name__ == '__main__':
 
     eel.init('web', ['.html', '.css'])
     eel.start('index.html', size=(720, 720))
+=======
+    # Return the dictionary as JSON
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
+# while True:
+#     current = spotify_object.currently_playing()
+#     status = current['currently_playing_type']
+
+#     if status == "track":
+#         artist_name = current['item']['album']['artists'][0]['name']
+#         song_title = current['item']['name']
+#         length = current['item']['duration_ms']
+#         progress = current['progress_ms']
+#         time_left = int(((length-progress)/1000))
+#         album_art_url = current['item']['album']['images'][0]['url']
+
+#         # Download the image and open it with PIL
+#         response = requests.get(album_art_url)
+#         image = Image.open(BytesIO(response.content))
+#         # Get the current time as a timestamp string
+#         timestamp = str(int(time.time()))
+
+#         # Construct the new file name with the timestamp
+#         new_file_name = 'img_' + timestamp + '.jpg'
+#         # Save the image to a file
+#         image.save(new_file_name)
+
+#         # song = genius.search_song(title=song_title, artist=artist_name)
+#         # lyrics = song.lyrics
+#        # print(lyrics)
+#         time.sleep(time_left)
+
+#     elif status == "ad":
+#         time.sleep(30)
+>>>>>>> 32224c1 (update)
---
+++ static/JS/displayCoverArt.js
@@ -0,0 +1,39 @@
+let prevAlbumArtUrl = ""; // Declare the variable outside the function
+function updateImage() {
+  fetch("/currently_playing_info")
+    .then((response) => response.json())
+    .then((data) => {
+      const artistName = data.artist_name;
+      const songTitle = data.song_title;
+      const albumName = data.album_name;
+      const length = data.length;
+      const progress = data.progress;
+      const albumArtUrl = data.album_art_url;
+      if (prevAlbumArtUrl !== albumArtUrl) {
+        // Use the values to update the UI
+        document.getElementById("artist").innerText = artistName;
+        document.getElementById("track").innerText = songTitle;
+        document.getElementById("album").innerText = albumName;
+
+        const albumArt = document.getElementById("album-art");
+        const albumBackground = document.getElementById("album-art-background");
+        albumArt.src = albumArtUrl; // Update the image source
+        // albumBackground.style.backgroundImage = `url(${albumArtUrl})`;
+
+        albumArt.style.opacity = 0; // Fade out the current image
+        albumBackground.style.opacity = 0;
+        albumArt.addEventListener("transitionend", () => {
+          // albumBackground.style.backgroundImage = `url(${albumArtUrl})`;
+          albumArt.onload = () => {
+            albumArt.style.opacity = 1; // Fade in the new image
+            albumBackground.style.opacity = 1;
+          };
+          albumArt.src = albumArtUrl; // Update the image source
+        });
+      }
+      prevAlbumArtUrl = albumArtUrl;
+    });
+}
+
+// Call the updateImage function every 5 seconds
+setInterval(updateImage, 5000);
